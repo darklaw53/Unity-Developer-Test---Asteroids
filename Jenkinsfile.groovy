288c3e60ed01ecae2a0ab47565c9ecded6421e08
darklaw53 <marceloschulze@gmail.com>
2025-02-17 09:41:53 -0300

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,7 +2,7 @@ def PROJECT_NAME = "Unity-Developer-Test---Asteroids"
 def CUSTOM_WORKSPACE = "C:\\Users\\Osten Games\\Documents\\GitHub\\Unity-Developer-Test---Asteroids\\${PROJECT_NAME}"
 def UNITY_VERSION = "2022.3.42f1"
 def UNITY_INSTALLATION = "D:\\unity\\${UNITY_VERSION}\\Editor"
-def UNITY_USERNAME = "marceloschulze@gmail.com"
+def UNITY_USERNAME = "marcelo"
 def UNITY_PASSWORD = "Marcelo53"
 
 pipeline {

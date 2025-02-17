a2e15a5cd591efe20b5c080bd4d8ced0682b692a
darklaw53 <marceloschulze@gmail.com>
2025-02-17 10:43:16 -0300

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,14 +1,14 @@
 def PROJECT_NAME = "Unity-Developer-Test---Asteroids"
-def CUSTOM_WORKSPACE = "C:\\Users\\Osten Games\\Documents\\GitHub\\Unity-Developer-Test---Asteroids"
+def CUSTOM_WORKSPACE = "C:\\Users\\Osten Games\\Documents\\GitHub\\Unity-Developer-Test---Asteroids\\${PROJECT_NAME}"
 def UNITY_VERSION = "2022.3.42f1"
 def UNITY_INSTALLATION = "D:\\unity\\${UNITY_VERSION}\\Editor"
 
 pipeline {
     environment {
-        PROJECT_PATH = "${CUSTOM_WORKSPACE}"
+        PROJECT_PATH = "${CUSTOM_WORKSPACE}\\${PROJECT_NAME}"
+        UNITY_LICENSE_PATH = "C:\\ProgramData\\Jenkins\\Unity\\Unity_lic.ulf"
         UNITY_USERNAME = "marceloschulze@gmail.com"
         UNITY_PASSWORD = "Marcelo53"
-        ALF_FILE_PATH = "${CUSTOM_WORKSPACE}\\Unity_lic.alf"  // Path to save the .alf file in the workspace
     }
 
     agent {
@@ -29,14 +29,10 @@ pipeline {
             steps {
                 script {
                     withEnv(["UNITY_PATH=${UNITY_INSTALLATION}"]) {
-                        try {
-                            bat '''
-                            echo "Requesting Unity license with activation token..."
-                            "%UNITY_PATH%/Unity.exe" -batchmode -logFile -quit -username "%UNITY_USERNAME%" -password "%UNITY_PASSWORD%" -nographics -manualLicenseFile "%ALF_FILE_PATH%"
-                            '''
-                        } catch (Exception e) {
-                            echo "Unity license activation failed, but continuing..."
-                        }
+                        bat '''
+                        echo "Requesting Unity license with activation token..."
+                        "%UNITY_PATH%/Unity.exe" -batchmode -logFile -quit -username "%UNITY_USERNAME%" -password "%UNITY_PASSWORD%" -nographics -returnlicense
+                        '''
                     }
                 }
             }

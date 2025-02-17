4cba8198fb7c5431d1a65c295df9a40486a857ae
darklaw53 <marceloschulze@gmail.com>
2025-02-17 10:16:06 -0300

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,5 +1,5 @@
 def PROJECT_NAME = "Unity-Developer-Test---Asteroids"
-def CUSTOM_WORKSPACE = "C:\\Users\\Osten Games\\Documents\\GitHub\\Unity-Developer-Test---Asteroids\\${PROJECT_NAME}"
+def CUSTOM_WORKSPACE = "C:\\Users\\Osten Games\\Documents\\GitHub\\Unity-Developer-Test---Asteroids\\${PROJECT_NAME}-proj"
 def UNITY_VERSION = "2022.3.42f1"
 def UNITY_INSTALLATION = "D:\\unity\\${UNITY_VERSION}\\Editor"
 
